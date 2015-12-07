2b44de76ce63a5875ec06103966fbf2b2770c48e
Paweł Ksieniewicz <pawel@ksienie.com>
2015-12-07 20:20:56 +0100

    Done

--- server.js
+++ server.js
@@ -1,37 +1,64 @@
-var http = require("http");
-var fs = require("fs");
+// Biblioteki
+var http = require("http")
+var fs = require("fs")
 
-const badArgumentsMessage = "Błędne argumenty";
-
-http.createServer(function(message, response) {
-	question = message.url.split("/");
-
-	if(question.length == 2 || question.length == 3) {
-		if (question[1] == "") reject(response, badArgumentsMessage);
-		else {
-			if (question.length == 2) {	// Pytanie
-				console.log("Pytanie");
-				console.log(question[1]);
-				reject(response, "Zapytałeś, ale spierdalaj");
-			}
-			else { 						// Ustawianie
-				if(question[2] == "") reject(response, badArgumentsMessage);
-				console.log("Ustawienie");
-				reject(response, "Chciałeś coś ustawić, ale spierdalaj");
-			}
-		}
-	}
-	else {
-		reject(response, badArgumentsMessage);
-	}
-}).listen(8888);
-console.log("Listening on port 8888");
-
-function reject(response, message){
-	response.writeHead(404, {"Content-Type": "text/plain"});
-	response.end("\t" + message + "\n");
-}
+// Komunikaty
+const badArgumentsMessage = "Wrong arguments"
+const notFoundMessage = "Key not found"
+const keySet = "Key set"
+const keyUnset = "Key unset"
 
 fs.readFile("./data.json", function(err, file){
 	data = JSON.parse(file);
 });
+
+http.createServer(function(message, response) {
+	// Rozbij łańcuch na elementy
+	question = message.url.split("/")
+
+	if(question.length == 2 || question.length == 3) {
+		if (question[1] == "") answer(response, 400, badArgumentsMessage)
+		else {
+			key = question[1]
+			if (question.length == 2) {	
+				// Pytanie o wartość klucza
+				value = data[key];
+				if (value) 	answer(response, 200, value)
+				else 		answer(response, 404, notFoundMessage)
+			}
+			else { 						
+				// Ustawianie klucza
+				if(question[2] == ""){
+					// Jeśli jest taki klucz, a nie podaliśmy wartości, usuwamy go
+					if(data[key])
+					{
+						delete data[key]
+						synchronize()
+						answer(response, 200, keyUnset)
+					}
+					else answer(response, 400, badArgumentsMessage)
+				}
+				else {
+					// Jeśli podaliśmy wartość, ustawiamy go
+					value = question[2];
+					data[key] = value
+					synchronize()
+					answer(response, 200, keySet)
+				}
+			}
+		}
+	}
+	else {
+		answer(response, 400, badArgumentsMessage);
+	}
+}).listen(8888);
+console.log("Listening on port 8888");
+
+function answer(response, code, message){
+	response.writeHead(code, {"Content-Type": "text/plain"});
+	response.end(message);
+}
+
+function synchronize() {
+	fs.writeFile("./data.json", JSON.stringify(data))
+}
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl http://localhost:8888/
-curl http://localhost:8888/what
-curl http://localhost:8888/abc/100
-curl http://localhost:8888/abc/
-curl http://localhost:8888/abc
-curl http://localhost:8888/asd/asd/asd
